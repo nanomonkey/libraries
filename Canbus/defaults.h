43846aa242a208fcfa662f6e21c09c5acc73c076
Scott Garrison <scott@allpowerlabs.org>
2012-11-17 22:13:03 -0800

    First commit

---
+++ Canbus/defaults.h
@@ -0,0 +1,16 @@
+#ifndef	DEFAULTS_H
+#define	DEFAULTS_H
+
+#define	P_MOSI	J,1
+#define	P_MISO	J,0
+#define	P_SCK	J,2
+
+#define	MCP2515_CS	H,2 
+#define	MCP2515_INT	J,6
+#define LED2_HIGH			B,0
+#define LED2_LOW			B,0
+
+#endif	// DEFAULTS_H
+
+
+
