50dfb2f69abb9f7a3a47098839b9ea4dd6811020
crashkopf <resin@g-cipher.net>
2014-02-21 11:12:41 -0800

    Continued improvements to VNH bridge driver

--- KSlibs/vnh_bridge.c
+++ KSlibs/vnh_bridge.c
@@ -2,8 +2,9 @@
 #include "vnh_bridge.h"
 #include "adc.h"
 
-
 void vnh_reset(vnh_s* v){
+	// Go into standby mode
+	vnh_standby(v);
 	// Set pins for switches and PWM as outputs
 	gpio_set_ddr(&v->mota);
 	gpio_set_ddr(&v->motb);
@@ -11,97 +12,187 @@ void vnh_reset(vnh_s* v){
 	// Set enable pins as inputs
 	gpio_clr_ddr(&v->ena);
 	gpio_clr_ddr(&v->enb);
-	// Go into standby mode
-	vnh_standby(v);
-	// Clear the sample buffer
-	int x;
-	for (x=0; x < VNH_SAMPLES; x++) {
-		v->avg.samples[x] = 0;
-	}
-	v->avg.head = 0;
-	v->avg.accum = 0;
+	// Reset averaging function
+	vnh_avg_reset(v);
+	// Reset status flags
+	v->status.fault_A = 0;
+	v->status.fault_B = 0;
+	v->status.limit = 0;
+	// Toggle motor outputs to clear any faults
+	gpio_set_pin(&v->mota);
+	gpio_set_pin(&v->motb);
+	gpio_clr_pin(&v->mota);
+	gpio_clr_pin(&v->motb);
 }
+
+void vnh_standby(vnh_s* v){
+	// Set status to stop PWM
+	v->status.mode = VNH_STANDBY;
+	// Disable A, B
+	gpio_clr_pin(&v->ena);
+	gpio_clr_pin(&v->enb);
+	// Reset PWM counters
+	vnh_pwm_reset(v);
+}
+
 void vnh_forward(vnh_s* v){
-	v->mod.duty = 0;
-	v->mod.count = 0;
+	// Momentarily go to standby
+	vnh_standby(v);
 	// A=hi, B=lo
 	gpio_set_pin(&v->mota);
 	gpio_clr_pin(&v->motb);	
 	// Enable outputs
 	gpio_set_pin(&v->ena);
 	gpio_set_pin(&v->enb);
-	// Turn on PWM
-	v->status.enabled = 1;
-}	
+	// Set status
+	v->status.mode = VNH_FORWARD;
+}
+	
 void vnh_reverse(vnh_s* v){
-	v->mod.duty = 0;
-	v->mod.count = 0;
+	// Momentarily go to standby
+	vnh_standby(v);
 	// A=lo, B=hi
 	gpio_clr_pin(&v->mota);
 	gpio_set_pin(&v->motb);
 	// Enable outputs
 	gpio_set_pin(&v->ena);
 	gpio_set_pin(&v->enb);
-	// Turn on PWM
-	v->status.enabled = 1;
+	// Set status
+	v->status.mode = VNH_REVERSE;
 }
+
 void vnh_brake(vnh_s* v){
-	// Max duty
-	v->mod.duty = v->mod.max;
+	// Momentarily go to standby
+	vnh_standby(v);
 	// A=lo, B=lo
 	gpio_clr_pin(&v->mota);
 	gpio_clr_pin(&v->motb);
 	// Enable outputs
 	gpio_set_pin(&v->ena);
 	gpio_set_pin(&v->enb);
-}
-void vnh_standby(vnh_s* v){
-	// Disable PWM
-	v->status.enabled = 0;
-	// Disable A, B
-	gpio_clr_pin(&v->ena);
-	gpio_clr_pin(&v->enb);
-	// Turn off PWM
-	gpio_clr_pin(&v->pwm);
-	// Toggle inputs to clear any faults
-	gpio_set_pin(&v->mota);
-	gpio_set_pin(&v->motb);
-	gpio_clr_pin(&v->mota);
-	gpio_clr_pin(&v->motb);
+	v->status.mode = VNH_BRAKE;
 }	
 
+void vnh_avg_reset(vnh_s* v){
+	int x;
+	// Clear sample buffer
+	for (x=0; x < VNH_SAMPLES; x++) {
+		v->avg.samples[x] = 0;
+	}
+	v->avg.head = 0;
+	v->avg.accum = 0;
+}
+
+void vnh_pwm_reset(vnh_s* v){
+	// Go straight to target duty in fixed mode
+	if (v->mod.mode == VNH_PWM_FIXED) 
+		v->mod.duty = v->mod.target;
+	// Otherwise, start at zero.  PWM code will ramp up
+	else v->mod.duty = 0;
+	// Reset cycle counter
+	v->mod.count = 0;
+	// Turn off PWM output
+	gpio_clr_pin(&v->pwm);
+}
+
 void vnh_tick(vnh_s* v){
+	static unsigned int adcpre = 0;
+	vnh_pwm_tick(v);
+	// ADC sampling is pre-scaled
+	if (++adcpre > 10){
+		vnh_adc_tick(v);
+		adcpre=0;
+	}
+	// Fault checks
+	v->status.fault_A = !gpio_get_pin(&v->ena);
+	v->status.fault_B = !gpio_get_pin(&v->enb);
+	// Current limiting
+	if (v->climit) {
+		// If we're over the limit current go into limit mode
+		if (v->avg.avg > v->climit) {
+			v->status.limit = 1;
+		}
+		// If we're in limit mode but no longer above the limit current
+		// (minus our hysteresis value), go back to normal mode
+		if (v->status.limit && (v->avg.avg < (v->climit - v->chyst))) {
+			v->status.limit = 0;
+		}
+	}
+}
+
+void vnh_pwm_tick(vnh_s* v){
+	vnh_status_s stat = v->status;
+	switch (stat.mode) {
+		case VNH_STANDBY:
+			// Just let it go...
+			gpio_clr_pin(&v->pwm);
+			break;
+		case VNH_BRAKE:
+			// Clamp down hard!
+			gpio_set_pin(&v->pwm);
+			break;
+		case VNH_FORWARD:
+		case VNH_REVERSE:
+		default:
+			// Everything else in modulation
+			if (v->mod.mode != VNH_PWM_NONE) {
+				if (v->mod.count < v->mod.duty) 
+					gpio_set_pin(&v->pwm);
+				else 
+					gpio_clr_pin(&v->pwm);
+				v->mod.count++;
+				if (v->mod.count > VNH_MOD_MAX)
+					v->mod.count = 0;
+				// Only change duty at the beginning of a cycle
+				if (v->mod.count == 0) {
+					// Ramp down in limit mode
+					if (v->status.limit) {
+						if (v->mod.duty < v->mod.ramp) v->mod.duty = 0;
+						else v->mod.duty -= v->mod.ramp;
+					}
+					// Otherwise, ramp towards target duty
+					else {
+						if (v->mod.duty < v->mod.target) {
+							if (v->mod.target - v->mod.duty < v->mod.ramp)
+								v->mod.duty = v->mod.target;
+							else v->mod.duty += v->mod.ramp;
+						}
+						else {
+							if (v->mod.duty - v->mod.target < v->mod.ramp)
+								v->mod.duty = v->mod.target;
+							else v->mod.duty -= v->mod.ramp;
+						}
+					}
+				}	
+			}
+			else gpio_set_pin(&v->pwm);
+			break;
+	}
+}
+void vnh_adc_tick(vnh_s* v){
 	if (v->avg.head < (VNH_SAMPLES - 1)) {
-		v->avg.accum -= v->avg.samples[++v->avg.head];						// Subtract sample at tail and advance head
-		v->avg.samples[v->avg.head] = ADC_ReadChanSync(v->csense_adc);	// Overwrite old sample
-		v->avg.accum += v->avg.samples[v->avg.head];						// Add the latest sample
+		v->avg.accum -= v->avg.samples[++v->avg.head];				// Subtract sample at tail and advance head
+		v->avg.samples[v->avg.head] = ADC_ReadChanSync(v->adc);		// Overwrite old sample
+		v->avg.accum += v->avg.samples[v->avg.head];				// Add the latest sample
 	}
 	else {
 		// Roll over
-		v->avg.accum -= v->avg.samples[0];									// Subtract sample at tail and advance head
-		v->avg.samples[0] = ADC_ReadChanSync(v->csense_adc);				// Overwrite old sample
-		v->avg.accum += v->avg.samples[0];									// Add the latest sample
+		v->avg.accum -= v->avg.samples[0];				// Subtract sample at tail and advance head
+		v->avg.samples[0] = ADC_ReadChanSync(v->adc);	// Overwrite old sample
+		v->avg.accum += v->avg.samples[0];				// Add the latest sample
 		v->avg.head = 0;
 	}
-	if (v->status.enabled) {
-		if (v->mod.count < v->mod.duty) gpio_set_pin(&v->pwm);
-		else gpio_clr_pin(&v->pwm);
-		v->mod.count++;
-		if (v->mod.count > v->mod.max) {
-			v->mod.count = 0;
-			v->mod.duty += v->mod.ramp;
-		}
-	}
-	else {
-		gpio_clr_pin(&v->pwm);
-	}
+	v->avg.avg = (v->avg.accum >> VNH_DIVISOR);		// Divide the accumulator
 }
 
 unsigned int vnh_get_current(vnh_s* v){
-	return (v->avg.accum >> VNH_DIVISOR);
+	return v->avg.avg;
 }
 vnh_status_s vnh_get_status(vnh_s *v){
 	return v->status;
+}
+uint8_t vnh_get_fault(vnh_s* v) {
+	return v->status.fault_A | v->status.fault_B | v->status.limit;
 }
 
 void vnh_set_period(vnh_s* v, unsigned int period){}
--- KSlibs/vnh_bridge.h
+++ KSlibs/vnh_bridge.h
@@ -4,31 +4,42 @@
 
 #include "gpio.h"
 
-#define VNH_UAMP_UNIT (37538)		// Unit for ADC to AMP conversion
-#define VNH_PWM_PERIOD ()
-#define VNH_SAMPLES (64)
-#define VNH_DIVISOR (6)
+#define VNH_ADC_UNIT (37538)		// Unit for ADC to mA conversion
+#define VNH_SAMPLES (64)			// Number of samples for AVG function
+#define VNH_DIVISOR (6)				// Bit shift divisor x, y / 2^x
+#define VNH_MOD_MAX (100)			// PWM counter maximum, 100 for duty in %
+#define VNH_FAULT_MASK (0x4C)
+
+#define VNH_STANDBY 0
+#define VNH_FORWARD 1
+#define VNH_REVERSE 2
+#define VNH_BRAKE 3
+#define VNH_LIMIT 1
+
+#define VNH_PWM_NONE 0
+#define VNH_PWM_FIXED 1
+#define VNH_PWM_SOFT 2
 
 typedef struct {
-	unsigned char enabled:1;			// 0=standby, 1=go
-	unsigned char direction:1;			// 0=FWD, 1=REV
-	unsigned char fault_A:1;			// Fault detected on channel A
-	unsigned char fault_B:1;			// Fault detected on channel B
-	unsigned char current:2;			// 0=LOW, 1=MED, 2=HIGH
-	unsigned char limit:1;				// 1 if over-current threshold reached
+	uint8_t mode:2;			// 0=STDBY, 1=FWD, 2=REV, 3=BRK
+	uint8_t fault_A:1;		// Fault detected on channel A
+	uint8_t fault_B:1;		// Fault detected on channel B
+	uint8_t limit:1;		// 1 if over-current threshold reached
 } vnh_status_s;
 
 typedef struct {
+	unsigned int avg;
 	unsigned int head;
 	unsigned long accum;
 	unsigned int samples[VNH_SAMPLES];
 } vnh_sbuf_s;
 
 typedef struct {
-	unsigned int max;
-	unsigned int duty;
-	unsigned int count;
-	unsigned int ramp;
+	uint8_t	mode;			// PWM mode: none, fixed, or soft start
+	uint8_t target;			// Target PWM duty
+	uint8_t duty;			// Current duty
+	uint8_t count;			// Cycle counter
+	uint8_t ramp;			// Ramp rate
 } vnh_mod_s;
 
 typedef struct {
@@ -38,10 +49,11 @@ typedef struct {
 	gpio_s ena;
 	gpio_s enb;
 	gpio_s pwm;
-	unsigned int csense_adc;	// ADC channel for current sense
-	unsigned int max_current;	// Current limit, in milliamps
-	vnh_mod_s mod;				// Modulation
-	vnh_sbuf_s avg;
+	unsigned int adc;		// ADC channel for current sense
+	unsigned int climit;	// Current limit, in ADC units
+	unsigned int chyst;		// Current limit hysteresis
+	vnh_mod_s mod;			// Modulation
+	vnh_sbuf_s avg;			// Averaging sample buffer
 } vnh_s;
 
 #ifdef __cplusplus
@@ -58,6 +70,10 @@ void vnh_tick(vnh_s*);
 
 unsigned int vnh_get_current(vnh_s*); // Returns average current
 vnh_status_s vnh_get_status(vnh_s*);
+uint8_t vnh_get_fault(vnh_s*);
+
+void vnh_avg_reset(vnh_s*);
+void vnh_pwm_reset(vnh_s*);
 
 #ifdef __cplusplus
 }
